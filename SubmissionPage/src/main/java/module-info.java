c7c0c857b9c48a24fe72933fa889be25d7188208
Sovichet-Rathanak <163481918+Sovichet-Rathanak@users.noreply.github.com>
2024-05-02 14:33:58 +0700

    Add files via upload

--- SubmissionPage/src/main/java/module-info.java
+++ SubmissionPage/src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module com.example.submissionpage {
+module org.example.submissionpage {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.example.submissionpage to javafx.fxml;
-    exports com.example.submissionpage;
+    opens org.example.submissionpage to javafx.fxml;
+    exports org.example.submissionpage;
 }
---
+++ SubmissionPage/src/main/java/org/example/submissionpage/Main.java
@@ -0,0 +1,26 @@
+package org.example.submissionpage;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setWidth(1440);
+        stage.setHeight(720);
+        stage.setResizable(false);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
